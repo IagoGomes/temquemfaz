e4e842075a8501619ab3c4f584a50e4b704d271e
IagoGomes <hiago_pacheco12@hotmail.com>
2017-03-15 22:15:13 -0300

    finalizado

--- src/core/Endereco.java
+++ src/core/Endereco.java
@@ -28,9 +28,30 @@ public class Endereco {
 
 	/**
 	 *  Método em que o construtor de endereço recebe todos os parâmetros menos o complemento.
+	 * @param rua String
+	 * @param cidade String
+	 * @param bairro String
+	 * @param cep int
+	 * @param estado String
+	 * @param pais String
+	 * @param num int
 	 */ 
-	public Endereco() {
+	public Endereco(String rua, String cidade, String bairro, int cep, String estado, String pais, int num) {
 
 	}
-
+        
+	/**
+	* Método que modifica o complemento de um endereço
+	*@param complemento String
+	*/
+	public void modificarComplemento(String complemento){
+		
+	}
+	
+	/**
+	*Método que defini um usuário para o endereço em questão
+	*@param usuario Usuario
+	*/
+	public void definirUsuario(Usuario usuario){
+	}
 }
