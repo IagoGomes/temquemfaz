e179120e7be8c789e6d95903c5504d4a2a65e329
IagoGomes <hiago_pacheco12@hotmail.com>
2017-03-15 21:51:51 -0300

    finalizado

--- src/core/Login.java
+++ src/core/Login.java
@@ -1,8 +1,9 @@
 package core;
 
 /**
- *
- * @author yanka
+ * Classe que valida um login de usuário no sistema.
+ * Assim, implementa todos os protocolos de segurança adotados na plataforma 
+ *   para validação de usuários
  */
 public class Login {
 
@@ -13,10 +14,10 @@ public class Login {
 	private Usuario usuario;
 
 	/**
-	 *  
-   * @param nickname
-   * @param senha
-   * @return 
+	 *  Método que valida o acesso de um usuário
+         * @param nickname : String
+         * @param senha : String
+         * @return boolean
 	 */
 	public boolean validar(String nickname, String senha) {
 		return false;
